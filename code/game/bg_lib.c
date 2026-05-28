47fb47edf4c101bbe5d39306ed1a934ce59a5e28
Brian Cullinan <112788814+briancullinan2@users.noreply.github.com>
2026-05-27 20:38:49 -0700

    Implement memcmp function for WASM

--- code/game/bg_lib.c
+++ code/game/bg_lib.c
@@ -300,6 +300,20 @@ void *memmove( void *dest, const void *src, size_t count ) {
 
 #if __WASM__
 
+
+int memcmp(const void *s1, const void *s2, size_t n) {
+    const unsigned char *p1 = s1, *p2 = s2;
+    while (n--) {
+        if (*p1 != *p2) {
+            return *p1 - *p2;
+        }
+        p1++;
+        p2++;
+    }
+    return 0;
+}
+
+
 double floor( double x ) {
 	return (int)(x + 0x40000000) - 0x40000000;
 }
